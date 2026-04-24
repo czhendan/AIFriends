f7bf8d92b5616d5c9a646a61afb1510353f548ae
chen zhendan <76423643+czhendan@users.noreply.github.com>
2026-04-24 16:40:47 +0800

    实现聊天框

---
+++ frontend/src/components/character/chat_field/ChatField.vue
@@ -0,0 +1,43 @@
+<script setup>
+import {computed, useTemplateRef} from "vue";
+import InputField from "@/components/character/chat_field/input_field/InputField.vue";
+import CharacterPhotoField from "@/components/character/chat_field/character_photo_field/CharacterPhotoField.vue";
+
+const props = defineProps(['friend'])
+const modelRef = useTemplateRef('model-ref')
+
+function showModal() {
+  modelRef.value.showModal()
+}
+
+const modalStyle = computed(() => {
+  if (props.friend) {
+    return {
+      backgroundImage: `url(${props.friend.character.background_image})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+    }
+  } else {
+    return {}
+  }
+})
+
+defineExpose({
+  showModal,
+})
+</script>
+
+<template>
+  <dialog ref="model-ref" class="modal">
+    <div class="modal-box w-90 h-150" :style="modalStyle">
+      <button @click="modelRef.close" class="btn btn-sm btn-ghost btn-circle bg-transparent absolute right-1 top-1">✕</button>
+      <InputField />
+      <CharacterPhotoField v-if="friend" :character="friend.character"/>
+    </div>
+  </dialog>
+</template>
+
+<style scoped>
+
+</style>
